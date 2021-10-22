4e37db6a7f05c613b9fcaab407f6e4b8f1875881
nahimaort <nahimaortega@gmail.com>
2021-10-22 09:33:59 +0100

    Kata3 versión 2

--- src/kata3/HistogramDisplay.java
+++ src/kata3/HistogramDisplay.java
@@ -1,6 +1,5 @@
 package kata3;
 
-import java.awt.Container;
 import java.awt.Dimension;
 import javax.swing.JPanel;
 import org.jfree.chart.ChartFactory;
@@ -11,15 +10,13 @@ import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
 
 public class HistogramDisplay extends ApplicationFrame {
+    private final Histogram<String> histogram;
 
-    public HistogramDisplay(String title) {
+    public HistogramDisplay(String title, Histogram<String> histogram) {
         super(title);
+        this.histogram = histogram;
         this.setContentPane(createPanel());
         this.pack();
-    }
-    
-    public void execute() {
-        this.setVisible(true);
     }
 
     private JPanel createPanel() {
@@ -36,12 +33,14 @@ public class HistogramDisplay extends ApplicationFrame {
     }
     
     private DefaultCategoryDataset createDataset() {
-        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
-        dataSet.addValue(5, "", "gmail.com");
-        dataSet.addValue(10, "", "ulpgc.es");
-        dataSet.addValue(7, "", "ull.es");
-        dataSet.addValue(2, "", "hotmail.com");
-        
-        return dataSet;
-    }   
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        for (String key : histogram.keySet()) {
+            dataset.addValue(this.histogram.get(key), "", key);
+        }
+        return dataset;
+    }
+    
+    public void execute() {
+        this.setVisible(true);
+    }
 }
